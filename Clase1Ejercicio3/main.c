168a595a6761db52ab10d6b9da89eb1f018b3a77
damiangeisser <dageisser@gmail.com>
2019-03-23 20:18:47 -0300

    0323

--- Clase1Ejercicio3/main.c
+++ Clase1Ejercicio3/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>//Permite el uso de caracteres de otros idiomas.
+#include <locale.h>//Permite el uso de símbolos de monedas y otros.
 
 int main()
 {
-    printf("Hello world!\n");
+    int i;
+    int numero;
+    int contadorNegativos = 0;
+    int contadorPositivos = 0;
+
+    setlocale(LC_ALL, "");//Activa el uso de caracteres de otros idiomas.
+
+    for(i=0; i<10; i++)
+    {
+
+        printf("Por favor ingrese un número y presione enter:");
+        scanf("%d", &numero);
+
+        if(numero>0)
+        {
+            contadorPositivos++;
+        }
+        else
+        {
+            if(numero<0)
+            {
+                contadorNegativos++;
+            }
+        }
+
+    }
+
+    printf("\nUsted ingresó %d números positivos y %d números negativos\n", contadorPositivos, contadorNegativos);
+
     return 0;
 }
