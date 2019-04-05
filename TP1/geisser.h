323f595d3aed8c06cc7525b622f5da9083f092b8
damiangeisser <dageisser@gmail.com>
2019-04-05 18:59:59 -0300

    Tarea 4 5

---
+++ TP1/geisser.h
@@ -0,0 +1,28 @@
+/**\brief Esta función suma dos valores flotantes devolviendo el resultado.
+ *
+ * \param sumando1 de tipo flotante, es el primer término de la suma.
+ * \param sumando2 de tipo flotante, es el segundo término de la suma.
+ * \return devuelve un flotante, resultado de la suma de los parámetros que recibe.
+ *
+ */
+float sumar(float sumando1, float sumando2);
+
+/**\brief Esta función resta dos valores flotantes devolviendo el resultado.
+ *
+ * \param minuendo de tipo flotante, es el primer término de la resta.
+ * \param sustraendo de tipo flotante, es el segundo término de la resta.
+ * \return devuelve un flotante, resultado de la resta de los parámetros que recibe.
+ *
+ */
+float restar(float minuendo, float sustraendo);
+
+/**\brief Esta función resta dos valores flotantes devolviendo el resultado.
+ *
+ * \param minuendo de tipo flotante, es el primer término de la resta.
+ * \param sustraendo de tipo flotante, es el segundo término de la resta.
+ * \return devuelve un flotante, resultado de la resta de los parámetros que recibe.
+ *
+ */
+float multiplicar(float factor1, float factor2);
+float dividir(float dividendo, float divisor);
+int factorial(int operandoFac);
--- TP1/main.c
+++ TP1/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>//Permite el uso de caracteres de otros idiomas.
-#include <locale.h>//Permite el uso de símbolos de monedas y otros.
+#include <wchar.h>//Permite el uso de caracteres latinos.
+#include <locale.h>//Permite el uso de caracteres latinos.
+#include "geisser.h"
 
 float sumar(float sumando1, float sumando2);
 float restar(float minuendo, float sustraendo);
@@ -20,17 +21,16 @@ int main()
     float rtaDiv;
     int rtaFactor1;
     int rtaFactor2;
-    int mostrarOk = 0;
     int errorDiv = 0;
     int errorFactor1 = 0;
     int errorFactor2 = 0;
     int mostrarOk = 0;
 
-    setlocale(LC_ALL, "");//Activa el uso de caracteres latinos
+    setlocale(LC_ALL, "");//Activa el uso de caracteres latinos.
 
     do
     {
-        printf("\nElija la acción a realizar y presione enter:\n\n1 - Ingresar el primer operando.\n\n2 - Ingresar el segundo operando.\n\n3 - Realizar todas las operaciones\n\n4 - Mostrar los resultados.\n\n5 - Salir.\n\n");
+        printf("\nPor favor elija la acción a realizar y presione enter:\n\n1 - Ingresar el primer operando.\n\n2 - Ingresar el segundo operando.\n\n3 - Realizar todas las operaciones.\n\n4 - Mostrar los resultados.\n\n5 - Salir.\n\n");
 
         scanf("%d", &opcion);
 
@@ -38,22 +38,19 @@ int main()
         {
 
         case 1:
-            printf("\nPor favor ingrese el 1er operando y presione enter:\n");
+            printf("\nPor favor ingrese el 1er operando y presione enter: ");
             scanf("%f", &operandoUno);
             break;
         case 2:
-            printf("\nPor favor ingrese el 2er operando y presione enter:\n");
+            printf("\nPor favor ingrese el 2do operando y presione enter: ");
             scanf("%f", &operandoDos);
             break;
         case 3:
-
-
             rtaSuma=sumar(operandoUno, operandoDos);
 
             rtaResta=restar(operandoUno, operandoDos);
 
             //Inicio división con validación
-
             if(operandoDos == 0)
             {
                 errorDiv = 1;
@@ -67,139 +64,95 @@ int main()
             rtaMulti=multiplicar(operandoUno, operandoDos);
 
             //Incio factorial del primer operando con validación.
-
-            if(operando1 < 1)
+            if(operandoUno < 1)
             {
                 errorFactor1 = 1;
             }
             else
             {
-
                 rtaFactor1 = factorial(operandoUno);
             }
-
             //Fin primer factorial
 
             //Inicio factorial del segundo operando con validación.
-
-            if(operando2 < 1)
+            if(operandoDos < 1)
             {
                 errorFactor2 = 1;
             }
             else
             {
-
                 rtaFactor2 = factorial(operandoDos);
             }
-
             //Fin segundo factorial
 
-            printf("\nOperaciones realizadas.\n\n");
+            printf("\n>>> Operaciones realizadas <<<\n\n");
 
             mostrarOk = 1;
             break;
         case 4:
             if(mostrarOk)
             {
-                printf("\nEl resultado de %f + %f es: %f\n\n", operandoUno, operandoDos, rtaSuma);
+                printf("\nEl resultado de %f + %f es: %f\n", operandoUno, operandoDos, rtaSuma);
 
-                printf("\nEl resultado de %f - %f es: %f\n\n", operandoUno, operandoDos, rtaResta);
+                printf("\nEl resultado de %f - %f es: %f\n", operandoUno, operandoDos, rtaResta);
 
-                //
+                //Inicio mensajes de la división.
+                if(errorDiv)
+                {
+                    printf("\nNo es posible dividir por cero.\n");
+                }
+                else
+                {
+                    printf("\nEl resultado de %f / %f es: %f\n", operandoUno, operandoDos, rtaDiv);
+                }
+                //Fin mensajes de la división.
 
+                printf("\nEl resultado de %f x %f es: %f\n", operandoUno, operandoDos, rtaMulti);
+
+                //Inicio mensajes del 1er factorial.
+                if(errorFactor1)
+                {
+                    printf("\nNo fue posible calcular el factorial del primer operando, debe ser mayor o igual a 1 ");
+                }
+                else
+                {
+                    printf("\nEl factorial de %0.f es: %d ", operandoUno, rtaFactor1);
+                }
+                //Fin mensajes del 1er factorial.
+
+                //Inicio mensajes del 2do factorial.
+                if(errorFactor2)
+                {
+                    printf("y no fue posible calcular el factorial del segundo operando, debe ser mayor o igual a 1\n");
+                }
+                else
+                {
+                    printf("y el factorial de %0.f es: %d\n", operandoDos, rtaFactor2);
+                }
+                //Fin mensajes del 2do factorial.
+
+                //Reinicio de las variables.
                 operandoUno = 0;
                 operandoDos = 0;
+                //Reinicio la bandera de las operaciones.
+                mostrarOk = 0;
             }
             else
             {
-                printf("\nDebe elegir previamente la opción 3 para obtener los resultados a mostrar.\n\n");
+                printf("\n(!) Debe elegir previamente la opción 3\npara obtener los resultados a mostrar (!)\n\n");
             }
             break;
         case 5:
-            printf("\nPrograma finalizado.\n\n");
+            printf("\n----- Programa finalizado -----\n\n");
             break;
         default:
-            printf("\n%d no es una opción válida.\n\n", opcion);
+            printf("\n(!) %d no es una opción válida (!)\n\n", opcion);
         }
 
 
     }
     while(opcion!=5);
 
-    rtaSuma=sumar(5.5, 3.3);
-
-    rtaResta=restar(5.5, 3.3);
-
-    rtaMulti=multiplicar(5.5, 3.3);
-
-    rtaDiv=dividir(5.5, 3.3);
-
-    rtaFactor=factorial(3.9);
-
-    printf("\nEstos son los resultados: %f\n\n", rtaSuma);
-    printf("\nEstos son los resultados: %f\n\n", rtaResta);
-    printf("\nEstos son los resultados: %f\n\n", rtaMulti);
-    printf("\nEstos son los resultados: %f\n\n", rtaDiv);
-
-    printf("\nEstos son los resultados factor: %d\n\n", rtaFactor);
-
-
-
-//    rtaSuma = (sumar(operandoUno, operandoDos));
-//
-//    printf("El resultado de %.2f + %.2f es: %.2f", operandoUno, operandoDos, rtaSuma);
-//
-//    rtaResta = (restar(operandoUno, operandoDos));
-//
-//    printf("El resultado de %.2f - %.2f es: %.2f", operandoUno, operandoDos, rtaResta);
-
-
     return 0;
 }
 
-float sumar(float sumando1, float sumando2)
-{
-    float rtaFuncionSum;
-
-    rtaFuncionSum = sumando1 + sumando2;
-
-    return rtaFuncionSum;
-}
-
-float restar(float minuendo, float sustraendo)
-{
-    float rtaFuncionRes;
-
-    rtaFuncionRes = minuendo - sustraendo;
-
-    return rtaFuncionRes;
-}
-
-float multiplicar(float factor1, float factor2)
-{
-    float rtaFuncionMulti;
-
-    rtaFuncionMulti = factor1 * factor2 ;
-
-    return rtaFuncionMulti;
-}
-
-float dividir(float dividendo, float divisor)
-{
-    float rtaFuncionDiv;
-
-    rtaFuncionDiv = dividendo / divisor ;
-
-    return rtaFuncionDiv;
-}
-
-int factorial(int operandoFac)
-{
-    int rtaFuncionFactor=1;
-
-    for(operandoFac; operandoFac>1; operandoFac--)
-    {
-        rtaFuncionFactor = rtaFuncionFactor * operandoFac;
-    }
-    return rtaFuncionFactor;
-}
